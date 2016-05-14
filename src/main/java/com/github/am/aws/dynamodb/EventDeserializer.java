0924bd3a1cbf50e3bb45175193beaf76e3e46a7b
Aman Manocha <Aman Manocha>
2016-05-14 09:29:00 +0530

    Formatting

--- src/main/java/com/github/am/aws/dynamodb/EventDeserializer.java
+++ src/main/java/com/github/am/aws/dynamodb/EventDeserializer.java
@@ -11,50 +11,50 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 
 public class EventDeserializer {
-	private static final String BINARY_TYPE = "\\{\"B\":";
-	private static final String NUMBER_TYPE = "\\{\"N\":";
-	private static final String STRING_TYPE = "\\{\"S\":";
+  private static final String BINARY_TYPE = "\\{\"B\":";
+  private static final String NUMBER_TYPE = "\\{\"N\":";
+  private static final String STRING_TYPE = "\\{\"S\":";
 
-	private static final String RECORDS_NODE = "Records";
-	private static final String DYNAMODB_NODE = "dynamodb";
-	private static final String NEW_IMAGE_NODE = "NewImage";
+  private static final String RECORDS_NODE = "Records";
+  private static final String DYNAMODB_NODE = "dynamodb";
+  private static final String NEW_IMAGE_NODE = "NewImage";
 
-	private Gson gson = new Gson();
+  private Gson gson = new Gson();
 
-	public <T> List<T> deserialize(String jsonString, Class<T> clazz) throws IOException {
-		JsonNode json = new ObjectMapper().readTree(jsonString);
-		JsonNode recordNodes = json.get(RECORDS_NODE);
+  public <T> List<T> deserialize(String jsonString, Class<T> clazz) throws IOException {
+    JsonNode json = new ObjectMapper().readTree(jsonString);
+    JsonNode recordNodes = json.get(RECORDS_NODE);
 
-		if (recordNodes == null)
-			return emptyList();
+    if (recordNodes == null)
+      return emptyList();
 
-		return deserialize(clazz, recordNodes);
-	}
+    return deserialize(clazz, recordNodes);
+  }
 
-	private <T> List<T> deserialize(Class<T> clazz, JsonNode recordNodes) {
-		List<T> models = new ArrayList<>();
-		for (int i = 0; i < recordNodes.size(); i++) {
-			JsonNode recordNode = recordNodes.get(i);
-			JsonNode dynamodbNode = recordNode.get(DYNAMODB_NODE);
-			JsonNode newImageNode = dynamodbNode.get(NEW_IMAGE_NODE);
+  private <T> List<T> deserialize(Class<T> clazz, JsonNode recordNodes) {
+    List<T> models = new ArrayList<>();
+    for (int i = 0; i < recordNodes.size(); i++) {
+      JsonNode recordNode = recordNodes.get(i);
+      JsonNode dynamodbNode = recordNode.get(DYNAMODB_NODE);
+      JsonNode newImageNode = dynamodbNode.get(NEW_IMAGE_NODE);
 
-			models.add((T) deserializeNewImage("" + newImageNode, clazz));
-		}
-		return models;
-	}
+      models.add((T) deserializeNewImage("" + newImageNode, clazz));
+    }
+    return models;
+  }
 
-	private <T> T deserializeNewImage(String json, Class<T> clazz) {
-		String parts[] = json.split("}");
+  private <T> T deserializeNewImage(String json, Class<T> clazz) {
+    String parts[] = json.split("}");
 
-		String jsonWithoutTypes = "";
-		for (String part : parts) {
-			jsonWithoutTypes += part.replaceAll(STRING_TYPE, "");
-			jsonWithoutTypes = jsonWithoutTypes.replaceAll(NUMBER_TYPE, "");
-			jsonWithoutTypes = jsonWithoutTypes.replaceAll(BINARY_TYPE, "");
-		}
-		jsonWithoutTypes = jsonWithoutTypes + "}";
+    String jsonWithoutTypes = "";
+    for (String part : parts) {
+      jsonWithoutTypes += part.replaceAll(STRING_TYPE, "");
+      jsonWithoutTypes = jsonWithoutTypes.replaceAll(NUMBER_TYPE, "");
+      jsonWithoutTypes = jsonWithoutTypes.replaceAll(BINARY_TYPE, "");
+    }
+    jsonWithoutTypes = jsonWithoutTypes + "}";
 
-		return gson.fromJson(jsonWithoutTypes, clazz);
-	}
+    return gson.fromJson(jsonWithoutTypes, clazz);
+  }
 
 }
--- src/test/java/com/github/am/aws/dynamodb/EventDeserializerShould.java
+++ src/test/java/com/github/am/aws/dynamodb/EventDeserializerShould.java
@@ -8,9 +8,9 @@ import java.util.List;
 import org.junit.Test;
 
 public class EventDeserializerShould {
-	private EventDeserializer eventDeserializer = new EventDeserializer();
+  private EventDeserializer eventDeserializer = new EventDeserializer();
 
-	//@formatter:off
+  //@formatter:off
 	@Test public void
 	deserialize_dynamodb_new_image_request() throws Exception {
     List<TestType> deserializedModels = eventDeserializer.deserialize(sampleDynamoDbEvent(), TestType.class);
@@ -23,22 +23,22 @@ public class EventDeserializerShould {
 	}
 	//@formatter:on
 
-	private String sampleDynamoDbEvent() throws UnsupportedEncodingException {
-		return "{\"Records\":[{\"eventID\":\"491eea1491aab476cd1071927e53bca4\",\"eventName\":\"INSERT\",\"eventVersion\":\"1.0\",\"eventSource\":\"aws:dynamodb\",\"awsRegion\":\"ap-northeast-1\",\"dynamodb\":{\"Keys\":{\"Id\":{\"S\":\"1\"}},\"NewImage\":{\"token\":{\"S\":\"DUMMY_TOKEN\"},\"id\":{\"N\":\"1\"}},\"SequenceNumber\":\"73200000000001986199874\",\"SizeBytes\":13,\"StreamViewType\":\"NEW_AND_OLD_IMAGES\"},\"eventSourceARN\":\"arn:aws:dynamodb:ap-northeast-1:049428796662:table/advice/stream/2016-05-11T07:24:20.363\"}]}";
-	}
+  private String sampleDynamoDbEvent() throws UnsupportedEncodingException {
+    return "{\"Records\":[{\"eventID\":\"491eea1491aab476cd1071927e53bca4\",\"eventName\":\"INSERT\",\"eventVersion\":\"1.0\",\"eventSource\":\"aws:dynamodb\",\"awsRegion\":\"ap-northeast-1\",\"dynamodb\":{\"Keys\":{\"Id\":{\"S\":\"1\"}},\"NewImage\":{\"token\":{\"S\":\"DUMMY_TOKEN\"},\"id\":{\"N\":\"1\"}},\"SequenceNumber\":\"73200000000001986199874\",\"SizeBytes\":13,\"StreamViewType\":\"NEW_AND_OLD_IMAGES\"},\"eventSourceARN\":\"arn:aws:dynamodb:ap-northeast-1:049428796662:table/advice/stream/2016-05-11T07:24:20.363\"}]}";
+  }
 
-	class TestType {
-		int id;
-		String token;
+  class TestType {
+    int id;
+    String token;
 
-		public int getId() {
-			return id;
-		}
+    public int getId() {
+      return id;
+    }
 
-		public String getToken() {
-			return token;
-		}
+    public String getToken() {
+      return token;
+    }
 
-	}
+  }
 
 }
